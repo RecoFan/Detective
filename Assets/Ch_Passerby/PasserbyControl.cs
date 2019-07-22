cd033f90bf4580971b99868cd4580c1cad658695
XZMADAO <826021077@qq.com>
2019-07-23 00:12:33 +0800

    passerby

---
+++ Assets/Ch_Passerby/PasserbyControl.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PasserbyControl : MonoBehaviour
+{
+    public Animator anim;
+    public float MoveSpeed;
+    private int MoveDistance = 3200;
+    private int moving_dis = 0;
+    private int rest_time_remain = 0;
+    private int isRotating = 0;
+    private int rotateAngle = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        anim.SetBool("isWalking",false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(new Vector3(0, 0, speed * Time.deltaTime));
+            anim.SetBool("isWalking", true);
+        }
+        else if (Input.GetKey(KeyCode.S)) {
+            transform.Translate(new Vector3(0,0,-speed*Time.deltaTime));
+            anim.SetBool("isWalking", true);
+        }
+        else {
+            anim.SetBool("isWalking", false);
+        }*/
+        if (rest_time_remain == 0 && isRotating == 0)
+        {
+            anim.SetBool("isWalking", true);
+            transform.Translate(new Vector3(0, 0, MoveSpeed * Time.deltaTime));
+            moving_dis++;
+            if (moving_dis == (int)MoveDistance/MoveSpeed)
+            {
+                rest_time_remain = 20;
+            }
+        }
+        else if (isRotating == 0)
+        {
+            anim.SetBool("isWalking", false);
+            rest_time_remain--;
+            if (rest_time_remain == 0) {
+                isRotating = 1;
+                moving_dis = 0;
+            }
+        }
+        else {
+            anim.SetBool("isWalking", false);
+            transform.Rotate(Vector3.up, 10);
+            rotateAngle += 10;
+            if (rotateAngle == 180) {
+                rotateAngle = 0;
+                isRotating = 0;
+            }
+        }
+    }
+}

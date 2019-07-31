d4911595c71c552736934d62bc4a53efd895ea98
vigorweijia <vigorweijia@gmail.com>
2019-08-01 00:35:15 +0800

    Add Start Menu

---
+++ Assets/Script/GameBegin.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameBegin : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.GetComponent<Button>().onClick.AddListener(delegate () {
+            Global_Save.Instance.loadName = "FlashBack";
+            Application.LoadLevel("LoadingScene");
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
